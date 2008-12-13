73e593cdf045a6a7e2a2e2303099e499ff551be9
tbatchelli MBP <tbatchelli@acm.org>
2008-12-13 10:39:25 -0800

    -Added Maven deployment information - Created a Gant script (test) - Fixed gails-ui -- missing SimpleDateFormat.js file (apparently)

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,22 +1,23 @@
 import org.apache.commons.codec.digest.DigestUtils;
 
 class BootStrap {
-  def init = {servletContext ->
-    def pass = DigestUtils.md5Hex("pass")
-    def person = new Person(username: "tbatchelli", userRealName: "Antoni Batchelli",
-            email: "tbatchelli@acm.org", description: "tbatchelli's account",
-            passwd: pass, enabled: true).save()
-    def admin = new Person(username: "admin", userRealName: "Administrator",
-            email: "tbatchelli@acm.org", description: "Our admin", passwd: pass,
-            enabled: true).save()
-    def userAuth = new Authority(authority: "ROLE_USER", description: "Authenticated User").save()
-    def su = new Authority(authority: "ROLE_ADMIN", description: "Administrator Role").save()
-    userAuth.addToPeople(person)
-    su.addToPeople(admin)
-    new Requestmap(url: "/", configAttribute: "IS_AUTHENTICATED_ANONYMOUSLY").save()
-    new Requestmap(url: "/login/**", configAttribute: "IS_AUTHENTICATED_ANONYMOUSLY").save()
-    new Requestmap(url: "/**", configAttribute: "IS_AUTHENTICATED_REMEMBERED").save()
-  }
-  def destroy = {
-  }
-} 
+    def init = {servletContext ->
+        def pass = DigestUtils.md5Hex("pass")
+        def person = new Person(username: "tbatchelli", userRealName: "Antoni Batchelli",
+                email: "tbatchelli@acm.org", description: "tbatchelli's account",
+                passwd: pass, enabled: true).save()
+        def admin = new Person(username: "admin", userRealName: "Administrator",
+                email: "tbatchelli@acm.org", description: "Our admin", passwd: pass,
+                enabled: true).save()
+        def userAuth = new Authority(authority: "ROLE_USER", description: "Authenticated User").save()
+        def su = new Authority(authority: "ROLE_ADMIN", description: "Administrator Role").save()
+        userAuth.addToPeople(person)
+        su.addToPeople(admin)
+        new Requestmap(url: "/", configAttribute: "IS_AUTHENTICATED_ANONYMOUSLY").save()
+        new Requestmap(url: "/login/**", configAttribute: "IS_AUTHENTICATED_ANONYMOUSLY").save()
+        new Requestmap(url: "/**", configAttribute: "IS_AUTHENTICATED_REMEMBERED").save()
+//        new Requestmap(url: "/ticket/list", configAttribute:"IS_AUTHENTICATED_ANONIMOUSLY").save()
+    }
+    def destroy = {
+    }
+}
